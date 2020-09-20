5ec106633e578c7597d615f2d42cee6040351b29
Karl Kadalipp <karl.kadalipp@gmail.com>
2020-09-20 19:43:06 +0300

    Sending form data from frontend to backend

--- src/App.js
+++ src/App.js
@@ -12,40 +12,50 @@ import axios from "axios/index";
 import i18n from "./translations/i18n";
 
 function Page() {
-	const {t, i18n} = useTranslation();
-	const changeLanguage = lng => {
-		i18n.changeLanguage(lng);
-		localStorage.setItem('SelectedLanguage', lng);
-	};
 
-	const handleSubmit = event => {
-		alert("Form submitted!") // + this.state.value
-		event.preventDefault()
-	}
-
+	const apiRoot = 'http://localhost:8090/api/';
+	const urlSectors = apiRoot + 'sectors';
 	const langSelectItems = [
 		{label: 'English', value: 'en'},
 		{label: 'Eesti', value: 'et'}
 	];
 
-	const urlSectors = 'http://localhost:8090/api/sectors'
+	const initialData = Object.freeze({
+		loadingSectors: true,
+		id: 0,
+		username: "",
+		// allSectors: [],
+		selectedSectors: [],
+		agreedToTerms: false
+	});
+	let [formData, setFormData] = useState(initialData);
+	let [isLoadingSectors, setIsLoadingSectors] = useState(initialData.loadingSectors);
+	// let [allSectors, setAllSectors] = useState(initialData.allSectors);
+	let [selectedSectors, setSelectedSectors] = useState(initialData.selectedSectors);
 
 	useEffect(() => {
 		const savedLanguage = localStorage.getItem('SelectedLanguage') || 'en';
 		changeLanguage(savedLanguage);
 		getSectors();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	let [isLoadingSectors, setIsLoadingSectors] = useState(true);
-	let [sectors, setSectors] = useState([]);
+	const {t, i18n} = useTranslation();
+	const changeLanguage = lng => {
+		i18n.changeLanguage(lng).then(() => {
+					localStorage.setItem('SelectedLanguage', lng);
+				}
+		);
+	};
 
 	const getSectors = () => {
 		axios.get(urlSectors)
 				.then(res => res.data)
 				.then(data => {
 							setIsLoadingSectors(false);
-							setSectors(data);
-							// console.log("Sectors data is: ", data);
+							// setAllSectors(data);
+							setSelectedSectors(data);
+							// console.log("Sectors data is: ", data); // DEBUG
 						}
 				)
 				.catch((err) => {
@@ -53,8 +63,34 @@ function Page() {
 				}, [])
 	};
 
+	const handleChangeInput = (event) => {
+		console.log(formData);
+		setFormData({
+			...formData,
+			[event.target.name]: event.target.value.trim()
+		});
+	};
+
+	const handleChangeSelect = (event) => {
+		let selectedOptions = [...event.target.options].filter(o => o.selected).map(o => o.value);
+		console.log("SELECTED SECTORS ARE " + JSON.stringify(selectedOptions));
+		setFormData({
+			...formData,
+			selectedSectors: selectedOptions
+		});
+		console.log(formData);
+	};
+
+	const handleChangeCheckbox = (event) => {
+		setFormData({
+			...formData,
+			[event.target.name]: event.target.checked
+		});
+		// console.log(formData); // DEBUG
+	};
+
 	const SectorsList = ({data}) => {
-		return <select multiple size="30">>
+		return <select multiple size="30" value={selectedSectors} onChange={handleChangeSelect}>
 			<React.Fragment>{createSectorsTable(data)}</React.Fragment>
 		</select>
 	}
@@ -73,6 +109,23 @@ function Page() {
 					</React.Fragment>
 				}
 		);
+	}
+
+	const handleSubmit = event => {
+		console.log(formData);
+		axios.post(apiRoot + 'save', {
+			id: formData.id,
+			username: formData.username,
+			selectedSectors: formData.selectedSectors,
+			agreedToTerms: formData.agreedToTerms
+		})
+				.then(function (response) {
+					console.log(response);
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		event.preventDefault()
 	}
 
 	return <div className="app">
@@ -98,23 +151,24 @@ function Page() {
 							<div className="data-block">
 								<label>
 									<span className="row-title unselectable">{t('content.name')}</span>
-									<input type="text"/>
+									<input type="text" name="username" onChange={handleChangeInput}/>
 								</label>
 							</div>
 							<div className="data-block">
 								<label>
 									<span className="row-title unselectable align-top">{t('content.sectors')}</span>
-									<SectorsList data={sectors}/>
+									{/*<SectorsList data={allSectors}/>*/}
+									<SectorsList data={selectedSectors}/>
 								</label>
 							</div>
 							<div className="data-block">
 								<label>
-									<input type="checkbox" className="row-title"/>
-									<span className="row-title unselectable">{t('content.agree-to-terms')}</span>
+									<input type="checkbox" name="agreedToTerms" className="row-title" onChange={handleChangeCheckbox}/>
+									<span className="row-title unselectable">{t('content.agreed-to-terms')}</span>
 								</label>
 							</div>
 							<div className="submit-block">
-								<Button label="Submit"/>
+								<Button label={t('content.save')}/>
 							</div>
 						</div>
 					</form>
